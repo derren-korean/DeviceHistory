048b7651560edc8d4e0602510d1be1b541897338
derren-korean <derren832@gamil.com>
2024-09-27 10:14:51 +0900

    init

---
+++ app/src/main/java/com/derren/devicehistory/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.derren.devicehistory
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.derren.devicehistory.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkBiometricAuthentication()
+    }
+    private fun checkBiometricAuthentication() {
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                // 지문 인증 가능한 경우
+                showBiometricPrompt()
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Toast.makeText(this, "기기에 생체 인증 하드웨어가 없습니다.", Toast.LENGTH_SHORT).show()
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Toast.makeText(this, "생체 인증 하드웨어를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                Toast.makeText(this, "생체 인증 정보가 등록되어 있지 않습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showBiometricPrompt() {
+        val executor = ContextCompat.getMainExecutor(this)
+
+        val biometricPrompt = androidx.biometric.BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    // 인증 성공 시 네비게이션 설정
+                    setupNavigation()
+                }
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext, "인증 오류: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                    finish() // 인증 실패 시 앱 종료
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "지문 인증 실패", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("지문 인증 필요")
+            .setSubtitle("앱에 접속하려면 지문 인증이 필요합니다.")
+            .setNegativeButtonText("취소")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun setupNavigation() {
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
